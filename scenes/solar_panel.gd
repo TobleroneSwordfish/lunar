9d2bbc01f8a409cb80ab4dcbe6d4fad0a8573bef
TobleroneSwordfish <jonathan.hall859@gmail.com>
2021-08-31 19:12:29 +0100

    fixed solar panel crash with export var

--- scenes/solar_panel.gd
+++ scenes/solar_panel.gd
@@ -3,9 +3,8 @@ class_name SolarPanel
 
 onready var respurce_container = $ResourceContainer
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func _init():
+	variant_name = "Solar panel"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
