856c4204e3a913e15617e6b7b2988937bba31df9
TobleroneSwordfish <jonathan.hall859@gmail.com>
2021-08-31 18:06:14 +0100

    prelim work on solar panels

---
+++ scenes/solar_panel.gd
@@ -0,0 +1,12 @@
+extends Construct
+class_name SolarPanel
+
+onready var respurce_container = $ResourceContainer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
